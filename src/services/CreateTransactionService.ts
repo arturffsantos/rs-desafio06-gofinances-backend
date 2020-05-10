fbd3947685be42e89b21fbe25d2e9e0cfdb7ecfb
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-05-10 12:57:48 -0300

    feat(transactions): endpoint to create transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,60 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: 'string';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category: categoryTitle,
+  }: Request): Promise<Transaction> {
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Invalid type. Must be income or outcome', 404);
+    }
+
+    const categoriesRepository = getRepository(Category);
+
+    const categoryExists = await categoriesRepository
+      .createQueryBuilder()
+      .where('LOWER(title) = LOWER(:categoryTitle)', { categoryTitle })
+      .getOne();
+
+    let category: Category;
+
+    if (categoryExists) {
+      category = categoryExists;
+    } else {
+      category = categoriesRepository.create({
+        title: categoryTitle,
+      });
+
+      category = await categoriesRepository.save(category);
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+
+    await transactionsRepository.save(transaction);
+    transaction.category = category;
+
+    return transaction;
   }
 }
 
