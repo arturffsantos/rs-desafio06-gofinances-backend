80b066c42d465cd0b3789890f0e43b61bccd3f56
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-05-10 21:48:26 -0300

    feat(transactions): import csv file to create transactions

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,42 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-await-in-loop */
+import path from 'path';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+import uploadConfig from '../config/upload';
+import loadCSV from '../utils/loadCSV';
+
+interface Request {
+  transactionsFilename: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ transactionsFilename }: Request): Promise<Transaction[]> {
+    const csvFilePath = path.resolve(
+      uploadConfig.directory,
+      transactionsFilename,
+    );
+
+    const data = (await loadCSV(csvFilePath)).map(transaction => {
+      return {
+        title: transaction[0],
+        type: transaction[1] as 'income' | 'outcome',
+        value: parseFloat(transaction[2]),
+        category: transaction[3],
+      };
+    });
+
+    const createTransactionService = new CreateTransactionService();
+
+    const transactions: Transaction[] = [];
+
+    for (let i = 0; i < data.length; i++) {
+      const transaction = await createTransactionService.execute(data[i]);
+      transactions.push(transaction);
+    }
+
+    return transactions;
   }
 }
 
